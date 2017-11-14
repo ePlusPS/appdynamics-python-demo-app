a479fa4561348fecb0d738ca73f2ecac7a7446cb
Gopalasivam <gpalaniappan@eplus.com>
2017-11-14 14:10:32 -0800

    Modified app config variables

--- demo/config.py
+++ demo/config.py
@@ -1,15 +1,15 @@
 import os
 
 MYSQL_DSN = {
-    'user': os.getenv('DEMO_MYSQL_USER', 'test'),
-    'password': os.getenv('DEMO_MYSQL_PASSWORD', 'test'),
-    'host': os.getenv('DEMO_MYSQL_HOST', 'localhost'),
-    'database': os.getenv('DEMO_MYSQL_DB', 'test'),
+    'user': os.getenv('DEMO_MYSQL_USER', '<mysql_db_username>'),
+    'password': os.getenv('DEMO_MYSQL_PASSWORD', '<mysql_db_password>'),
+    'host': os.getenv('DEMO_MYSQL_HOST', '<mysql_db_host>'),
+    'database': os.getenv('DEMO_MYSQL_DB', '<mysql_db_name>'),
 }
 
 PGSQL_DSN = {
-    'user': os.getenv('DEMO_PGSQL_USER', 'test'),
-    'password': os.getenv('DEMO_PGSQL_PASSWORD', 'test'),
-    'host': os.getenv('DEMO_PGSQL_HOST', '127.0.0.1'),
-    'database': os.getenv('DEMO_PGSQL_DB', 'test'),
+    'user': os.getenv('DEMO_PGSQL_USER', '<postgresql_db_username>'),
+    'password': os.getenv('DEMO_PGSQL_PASSWORD', '<postgresql_db_password>'),
+    'host': os.getenv('DEMO_PGSQL_HOST', '<postgresql_db_host>'),
+    'database': os.getenv('DEMO_PGSQL_DB', '<postgresql_db_name>'),
 }
